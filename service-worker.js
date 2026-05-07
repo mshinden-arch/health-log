e59a19efc752586cfaf1a42529f419e16324192d
mshinden-arch <m.shinden@kkelan.com>
2026-05-07 09:56:15 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,38 @@
-const CACHE_NAME = 'health-log-v1';
+const CACHE_NAME = 'poop-log-v6';
+
 const ASSETS = [
   './',
-  './index.html',
-  './manifest.json'
+  './index.html?v=6',
+  './manifest.json?v=6',
+  './icon.png?v=6'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
